31aea1e6338905cad534f273a3be575d9729485e
ElvisLee <elvislee0725@gmail.com>
2020-10-22 16:58:58 -0700

    my solution

--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -1,16 +1,40 @@
 import React from 'react';
 
 class EmployeesList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.employeeFilter = this.employeeFilter.bind(this);
+    this.state = {
+      filteredEmployees: this.props.employees,
+      inputVal: ''
+    }
+  }
+
+  employeeFilter() {
+    const { employees } = this.props;
+    this.setState({
+      filteredEmployees: employees.filter((e) => e.name.toLowerCase().includes(this.state.inputVal.toLowerCase()))
+    });
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    
+    this.setState({
+      [name]: value
+    }, this.employeeFilter);
+  }
 
   render() {
-    const { employees } = this.props;
+    const { filteredEmployees, inputVal } = this.state;
     return (
       <React.Fragment>
         <div className="controls">
-          <input type="text" className="filter-input" data-testid="filter-input" />
+          <input type="text" className="filter-input" data-testid="filter-input" value={inputVal} name="inputVal" onChange={this.handleChange}/>
         </div>
         <ul className="employees-list">
-          { employees.map(employee => (
+          { filteredEmployees.map(employee => (
             <li key={employee.name} data-testid="employee">{employee.name}</li>
           ))}
         </ul>
